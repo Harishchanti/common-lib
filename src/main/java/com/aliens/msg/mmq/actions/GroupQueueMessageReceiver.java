1996a0ef6e4075dc08cf15030268f1c6dcddc6e6
kiranpatil <kiranpatil@arvindinternet.com>
2016-09-26 15:06:13 +0530

    send msg to mycroft

--- src/main/java/com/aliens/msg/mmq/actions/GroupQueueMessageReceiver.java
+++ src/main/java/com/aliens/msg/mmq/actions/GroupQueueMessageReceiver.java
@@ -1,10 +1,13 @@
 package com.aliens.msg.mmq.actions;
 
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
 import com.aliens.msg.mmq.Message;
 import com.aliens.msg.mmq.Status;
 import com.rabbitmq.client.AMQP;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
 
 /**
  * Created by jayant on 25/9/16.
@@ -13,9 +16,21 @@ import org.springframework.stereotype.Component;
 @Scope("prototype")
 public class GroupQueueMessageReceiver extends MessageReceiver {
 
-    @Override
-    public Status action(Message message, AMQP.BasicProperties properties) throws Exception {
-        //call mycroft here
-        return Status.SUCCESS;
-    }
+	@Autowired
+	SendMessageToMycroft sendMessageToMycroft;
+
+	@Override
+	public Status action(Message message, AMQP.BasicProperties properties) throws Exception {
+		// call mycroft here
+		try {
+
+			JSONObject mycroftPayload = new JSONObject(message.getPayload());
+			String response = sendMessageToMycroft.withMycroftPayload(mycroftPayload).invoke();
+
+			return Status.SUCCESS;
+		} catch (Exception ex) {
+			return Status.FAILED;
+
+		}
+	}
 }
