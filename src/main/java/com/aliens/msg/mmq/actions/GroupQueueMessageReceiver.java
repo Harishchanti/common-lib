f88ed21714647acddbc1de8fd4a59452d911e5ec
jayant <jayantbit1@gmail.com>
2016-09-26 14:18:26 +0530

    saving in inbound messages

--- src/main/java/com/aliens/msg/mmq/actions/GroupQueueMessageReceiver.java
+++ src/main/java/com/aliens/msg/mmq/actions/GroupQueueMessageReceiver.java
@@ -1,8 +1,11 @@
 package com.aliens.msg.mmq.actions;
 
+import com.aliens.hipster.domain.InboundMessages;
+import com.aliens.hipster.repository.InboundMessagesRepository;
 import com.aliens.msg.mmq.Message;
 import com.aliens.msg.mmq.Status;
 import com.rabbitmq.client.AMQP;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -13,10 +16,17 @@ import org.springframework.stereotype.Component;
 @Scope("prototype")
 public class GroupQueueMessageReceiver extends MessageReceiver {
 
+    @Autowired
+    InboundMessagesRepository inboundMessagesRepository;
+
     @Override
     public Status action(Message message, AMQP.BasicProperties properties) throws Exception {
         //call mycroft here
-        Thread.sleep(1000);
+        InboundMessages inboundMessages= new InboundMessages();
+        inboundMessages.setGroupId(message.getGroupId());
+        inboundMessages.setMessageId(message.getMessageId());
+        inboundMessagesRepository.save(inboundMessages);
+        //Thread.sleep(1000);
         return Status.SUCCESS;
     }
 }
