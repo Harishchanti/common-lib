03bbac3658c625bf24fa6079de48584663895f92
kiranpatil <kiranpatil@arvindinternet.com>
2016-09-28 15:43:19 +0530

    test push

--- src/main/java/com/aliens/msg/mmq/ThreadWrapper.java
+++ src/main/java/com/aliens/msg/mmq/ThreadWrapper.java
@@ -51,9 +51,6 @@ public class ThreadWrapper {
             IntStream.range(0,1).forEach( (x)->executorService.submit(mainQueueWorkerProvider.get().withClient(client)));
             IntStream.range(0,client.getConsumer_count()).forEach( (x)->executorService.submit(groupQueueWorkerProvider.get().withClient(client)));
     	    cacheManager.updateSet(Constants.CLIENTS,client.getName());
-
-            
-
             IntStream.range(0,10)
                 .forEach( (x)->executorService.submit(
                     testMessageSenderProvider.get().withGroupId("g"+"_"+client.getName()+String.valueOf(x))));
