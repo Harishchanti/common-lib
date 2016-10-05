579736567c72514a7b7ca4e1fb6dd448be53e773
jayant <jayantbit1@gmail.com>
2016-10-06 01:37:49 +0530

    jar getting build module getting dependencies

---
+++ common/src/main/java/com/aliens/common/Dummy.java
@@ -0,0 +1,17 @@
+package com.aliens.common;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by jayant on 6/10/16.
+ */
+
+@Data
+@Component
+public class Dummy {
+
+    public static void main(String[] args) {
+
+    }
+}
