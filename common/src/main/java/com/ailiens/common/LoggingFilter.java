c6832425f66617d5d07faabbc1526c25d8c69cc3
jayant <jayantbit1@gmail.com>
2016-12-01 18:18:27 +0530

    Logging filter 0.0.2

--- common/src/main/java/com/ailiens/common/LoggingFilter.java
+++ common/src/main/java/com/ailiens/common/LoggingFilter.java
@@ -19,6 +19,9 @@ public class LoggingFilter implements Filter {
     public static final String REQ_ID_MDC ="REQ-ID";
     public static final String REQ_ID_HEADER="X-REQ-ID";
 
+    public static final String TRACE_ID_MDC ="TRACE-ID";
+    public static final String TRACE_ID_HEADER="X-TRACE-ID";
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
 
@@ -31,6 +34,15 @@ public class LoggingFilter implements Filter {
         {
             MDC.put(REQ_ID_MDC,reqId);
         }
+        else MDC.put(REQ_ID_MDC,UUID.randomUUID().toString());
+
+        String traceId=((HttpServletRequest)request).getHeader(TRACE_ID_HEADER);
+        if(!Strings.isNullOrEmpty(traceId))
+        {
+            MDC.put(TRACE_ID_MDC,traceId);
+        }
+        else MDC.put(TRACE_ID_MDC,UUID.randomUUID().toString());
+
         chain.doFilter(request,response);
         MDC.clear();
     }
@@ -53,5 +65,16 @@ public class LoggingFilter implements Filter {
         }
 
        return reqId;
+    }
+
+    public static String  getTraceId()
+    {
+        Object obj= MDC.get(TRACE_ID_MDC);
+
+        if(obj!=null) {
+            return obj.toString();
+        }
+
+        else return UUID.randomUUID().toString();
     }
 }
