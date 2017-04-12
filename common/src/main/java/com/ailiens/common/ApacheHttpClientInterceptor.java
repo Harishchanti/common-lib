16c97475f87082cf0b9a81d666ce6b99f9f16bc2
harish.v <harish.v@arvindinternet.com>
2017-04-12 15:39:52 +0530

    Adding Apache Http client Interceptor

---
+++ common/src/main/java/com/ailiens/common/ApacheHttpClientInterceptor.java
@@ -0,0 +1,30 @@
+package com.ailiens.common;
+
+import static com.ailiens.common.LoggingFilter.REQ_ID_HEADER;
+import static com.ailiens.common.LoggingFilter.TRACE_ID_HEADER;
+
+import java.io.IOException;
+
+import org.apache.http.Header;
+import org.apache.http.HttpException;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpRequestInterceptor;
+import org.apache.http.protocol.HttpContext;
+
+public class ApacheHttpClientInterceptor implements HttpRequestInterceptor {
+
+	@Override
+	public void process(HttpRequest request, HttpContext context) throws HttpException, IOException {
+
+		Header[] traceIds = request.getHeaders(TRACE_ID_HEADER);
+		Header[] requestIds = request.getHeaders(REQ_ID_HEADER);
+		if (traceIds.length == 0) {
+			request.addHeader(TRACE_ID_HEADER, LoggingFilter.getTraceId());
+		}
+		if (requestIds.length == 0) {
+			request.addHeader(REQ_ID_HEADER, LoggingFilter.getRequestId());
+		}
+		System.out.println(request.getAllHeaders());
+	}
+
+}
--- common/src/main/java/com/ailiens/common/LoggingFilter.java
+++ common/src/main/java/com/ailiens/common/LoggingFilter.java
@@ -1,14 +1,21 @@
 package com.ailiens.common;
 
-import com.google.common.base.Strings;
+import java.io.IOException;
+import java.util.UUID;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.UUID;
+import com.google.common.base.Strings;
 
 /**
  * Created by jayant on 26/11/16.
@@ -17,67 +24,64 @@ import java.util.UUID;
 @Component
 public class LoggingFilter implements Filter {
 
-    public static final String REQ_ID_MDC ="REQ-ID";
-    public static final String REQ_ID_HEADER="X-REQ-ID";
+	public static final String REQ_ID_MDC = "REQ-ID";
+	public static final String REQ_ID_HEADER = "X-REQ-ID";
 
-    public static final String TRACE_ID_MDC ="TRACE-ID";
-    public static final String TRACE_ID_HEADER="X-TRACE-ID";
+	public static final String TRACE_ID_MDC = "TRACE-ID";
+	public static final String TRACE_ID_HEADER = "X-TRACE-ID";
 
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
 
-    }
+	}
 
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        String reqId=((HttpServletRequest)request).getHeader(REQ_ID_HEADER);
-        if(!Strings.isNullOrEmpty(reqId))
-        {
-            MDC.put(REQ_ID_MDC,reqId);
-        }
-        else MDC.put(REQ_ID_MDC,UUID.randomUUID().toString());
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		String reqId = ((HttpServletRequest) request).getHeader(REQ_ID_HEADER);
+		if (!Strings.isNullOrEmpty(reqId)) {
+			MDC.put(REQ_ID_MDC, reqId);
+		} else
+			MDC.put(REQ_ID_MDC, UUID.randomUUID().toString());
 
-        String traceId=((HttpServletRequest)request).getHeader(TRACE_ID_HEADER);
-        if(!Strings.isNullOrEmpty(traceId))
-        {
-            MDC.put(TRACE_ID_MDC,traceId);
-        }
-        else MDC.put(TRACE_ID_MDC,UUID.randomUUID().toString());
+		String traceId = ((HttpServletRequest) request).getHeader(TRACE_ID_HEADER);
+		if (!Strings.isNullOrEmpty(traceId)) {
+			MDC.put(TRACE_ID_MDC, traceId);
+		} else
+			MDC.put(TRACE_ID_MDC, UUID.randomUUID().toString());
 
-        chain.doFilter(request,response);
-         HttpServletResponse httpServletResponse= (HttpServletResponse)response;
-        int status=httpServletResponse.getStatus();
-        MDC.clear();
-    }
+		chain.doFilter(request, response);
+		HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+		int status = httpServletResponse.getStatus();
+		MDC.clear();
+	}
 
-    @Override
-    public void destroy() {
-       MDC.clear();
-    }
+	@Override
+	public void destroy() {
+		MDC.clear();
+	}
 
+	public static String getRequestId() {
+		Object obj = MDC.get(REQ_ID_MDC);
+		String reqId = null;
+		if (obj != null) {
+			reqId = obj.toString();
+		}
+		if (Strings.isNullOrEmpty(reqId)) {
+			reqId = UUID.randomUUID().toString();
+		}
 
-    public static String  getRequestId()
-    {
-        Object obj= MDC.get(REQ_ID_MDC);
-        String reqId=null;
-        if(obj!=null) {
-            reqId = obj.toString();
-        }
-        if (Strings.isNullOrEmpty(reqId)) {
-            reqId = UUID.randomUUID().toString();
-        }
+		return reqId;
+	}
 
-       return reqId;
-    }
+	public static String getTraceId() {
+		Object obj = MDC.get(TRACE_ID_MDC);
 
-    public static String  getTraceId()
-    {
-        Object obj= MDC.get(TRACE_ID_MDC);
+		if (obj != null) {
+			return obj.toString();
+		}
 
-        if(obj!=null) {
-            return obj.toString();
-        }
-
-        else return UUID.randomUUID().toString();
-    }
+		else
+			return UUID.randomUUID().toString();
+	}
 }
