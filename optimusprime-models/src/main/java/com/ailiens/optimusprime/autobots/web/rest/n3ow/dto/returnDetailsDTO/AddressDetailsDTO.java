edc2e19208058c2a939f306a45083a9541dcfde4
jayant <jayant.mukherji@arvindinternet.com>
2017-09-04 15:03:17 +0530

    optimusprime-models 14

--- optimusprime-models/src/main/java/com/ailiens/optimusprime/autobots/web/rest/n3ow/dto/returnDetailsDTO/AddressDetailsDTO.java
+++ optimusprime-models/src/main/java/com/ailiens/optimusprime/autobots/web/rest/n3ow/dto/returnDetailsDTO/AddressDetailsDTO.java
@@ -12,37 +12,96 @@ public class AddressDetailsDTO {
 
     public String addressId;
 
-    public AddressDetailsDTO(OrderLine orderLine) {
-    	if(orderLine.getAddress() != null ){
-    		this.setAddressId(orderLine.getAddress().getAddressId());
-    	}
-	}
 
+    public String pincode = "";
+    private String line1 = "";
+    private String line2 = "";
+    private String line3 = "";
+    private String city = "";
+    private String state = "";
+    private String country = "";
 
     public AddressDetailsDTO() {
-        super();
+
+    }
+    public AddressDetailsDTO(OrderLine orderLine) {
+        if(orderLine.getAddress() != null ){
+            this.setAddressId(orderLine.getAddress().getAddressId());
+            this.setPincode(orderLine.getAddress().getPincode());
+            this.setLine1(orderLine.getAddress().getLine1());
+            this.setLine2(orderLine.getAddress().getLine2());
+            this.setLine3(orderLine.getAddress().getLine3());
+            this.setCity(orderLine.getAddress().getCity());
+            this.setState(orderLine.getAddress().getState());
+            this.setCountry(orderLine.getAddress().getCountry());
+        }
     }
 
 
-	public AddressDetailsDTO(Address address) {
-		this.setAddressId(address.getAddressId());
-	}
 
 
 
-	public String getAddressId() {
-		return addressId;
-	}
+    public String getPincode() {
+        return pincode;
+    }
+    public void setPincode(String pincode) {
+        this.pincode = pincode;
+    }
+    public String getLine1() {
+        return line1;
+    }
+    public void setLine1(String line1) {
+        this.line1 = line1;
+    }
+    public String getLine2() {
+        return line2;
+    }
+    public void setLine2(String line2) {
+        this.line2 = line2;
+    }
+    public String getLine3() {
+        return line3;
+    }
+    public void setLine3(String line3) {
+        this.line3 = line3;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getState() {
+        return state;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+    public String getCountry() {
+        return country;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    public AddressDetailsDTO(Address address) {
+        this.setAddressId(address.getAddressId());
+    }
 
 
 
-	public void setAddressId(String addressId) {
-		this.addressId = addressId;
-	}
+    public String getAddressId() {
+        return addressId;
+    }
 
 
 
-	@Override
+    public void setAddressId(String addressId) {
+        this.addressId = addressId;
+    }
+
+
+
+    @Override
     public String toString() {
         return ToStringBuilder.reflectionToString(this);
     }
