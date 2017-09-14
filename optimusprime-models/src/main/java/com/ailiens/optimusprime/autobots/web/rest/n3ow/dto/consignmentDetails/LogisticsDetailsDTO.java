4cab0db6416c0afaad02fce0dbd2748110db6404
aditya <aditya@arvininternet.com>
2017-09-14 13:21:58 +0530

    added payment trasnsaction details and courier details in consignment details

--- optimusprime-models/src/main/java/com/ailiens/optimusprime/autobots/web/rest/n3ow/dto/consignmentDetails/LogisticsDetailsDTO.java
+++ optimusprime-models/src/main/java/com/ailiens/optimusprime/autobots/web/rest/n3ow/dto/consignmentDetails/LogisticsDetailsDTO.java
@@ -6,170 +6,176 @@ import java.math.BigDecimal;
 import javax.annotation.Generated;
 import org.apache.commons.lang.builder.ToStringBuilder;
 
+import com.ailiens.optimusprime.domain.Courier;
+import com.ailiens.optimusprime.domain.Logistics;
 import com.ailiens.optimusprime.domain.OrderLine;
 
 @Generated("org.jsonschema2pojo")
 public class LogisticsDetailsDTO {
 
-    public String serviceType;
-    public String paymentType;
-    public BigDecimal CODAmount;
-    public String courierStatus;
-    public String lmsStatus;
-    public String courierName;
-    public String courierCode;
-    public String airwayBillNumber;
-    public String barCode;
+    public String serviceType = "";
+    public String paymentType = "";
+    public BigDecimal CODAmount = new BigDecimal(0.0);
+    public String courierStatus = "";
+    public String lmsStatus = "";
+    public String courierName="";
+    public String courierCode = "";
+    public String airwayBillNumber = "";
+    public String barCode = "";
     public String codBarCode;
-    public String promiseDate;
-    public String deliveryDate;
-    public String trackingLink;
-    public String dcCode;
-    public String shipmentCost;
-    public String codAirwayBillNumber;
-    public String fedexForwardShipmentType;
-    public String fedexReturnShipmentType;
-    public String fedexForwardFormId;
-    public String fedexReturnFormId;
-    public String fedexMeter;
-    public String countryCode;
-    public String cityCode;
-    public String fedexBillTc;
-    public String fedexBillDt;
+    public String promiseDate = "";
+    public String deliveryDate = "";
+    public String trackingLink = "";
+    public String dcCode = "";
+    public String shipmentCost="";
+    public String codAirwayBillNumber = "";
+    public String fedexForwardShipmentType = "";
+    public String fedexReturnShipmentType = "";
+    public String fedexForwardFormId = "";
+    public String fedexReturnFormId = "";
+    public String fedexMeter = "";
+    public String countryCode = "";
+    public String cityCode = "";
+    public String fedexBillTc = "";
+    public String fedexBillDt = "";
+    public CourierDetailDTO courierDetails;
 
     public LogisticsDetailsDTO() {
 
     }
     public LogisticsDetailsDTO(OrderLine orderLine) {
-		if(orderLine.getLogistics() !=null ){
-			this.setAirwayBillNumber(orderLine.getLogistics().getAwbNumber());
-			this.setLmsStatus(orderLine.getLogistics().getDeliveryStatus());
-			this.setServiceType(orderLine.getLogistics().getServiceType());
-			this.setPromiseDate(orderLine.getLogistics().getExpectedDeliveryDate());
-			this.setTrackingLink(orderLine.getLogistics().getLogisticsLink());
-			this.setCourierName(orderLine.getLogistics().getLogisticsPartner());
-            this.setBarCode(orderLine.getLogistics().getBarCode());
-            this.setCodBarCode(orderLine.getLogistics().getCodBarCode());
-            this.setDcCode(orderLine.getLogistics().getDcCode());
-            this.setCodAirwayBillNumber(orderLine.getLogistics().getCodAwbNumber());
-            this.setFedexForwardShipmentType(orderLine.getLogistics().getFedexForwardShipmentType());
-            this.setFedexReturnShipmentType(orderLine.getLogistics().getFedexReturnShipmentType());
-            this.setFedexForwardFormId(orderLine.getLogistics().getFedexForwardFormId());
-            this.setFedexReturnFormId(orderLine.getLogistics().getFedexReturnFormId());
-            this.setFedexMeter(orderLine.getLogistics().getFedexMeter());
-            this.setCountryCode(orderLine.getLogistics().getCountryCode());
-            this.setCityCode(orderLine.getLogistics().getCityCode());
-            this.setFedexBillTc(orderLine.getLogistics().getFedexBillTc());
-            this.setFedexBillDt(orderLine.getLogistics().getFedexBillDt());
-            if(orderLine.getLogistics().getDeliveryDate() != null)
-                this.setDeliveryDate(String.valueOf(orderLine.getLogistics().getDeliveryDate()));
-		}
-	}
+        if(orderLine.getLogistics() !=null ){
+            Logistics logistics = orderLine.getLogistics();
+            this.setAirwayBillNumber(logistics.getAwbNumber());
+            this.setLmsStatus(logistics.getDeliveryStatus());
+            this.setServiceType(logistics.getServiceType());
+            this.setPromiseDate(logistics.getExpectedDeliveryDate());
+            this.setTrackingLink(logistics.getLogisticsLink());
+            this.setCourierName(logistics.getLogisticsPartner());
+            this.setBarCode(logistics.getBarCode());
+            this.setCodBarCode(logistics.getCodBarCode());
+            this.setDcCode(logistics.getDcCode());
+            this.setCodAirwayBillNumber(logistics.getCodAwbNumber());
+            this.setFedexForwardShipmentType(logistics.getFedexForwardShipmentType());
+            this.setFedexReturnShipmentType(logistics.getFedexReturnShipmentType());
+            this.setFedexForwardFormId(logistics.getFedexForwardFormId());
+            this.setFedexReturnFormId(logistics.getFedexReturnFormId());
+            this.setFedexMeter(logistics.getFedexMeter());
+            this.setCountryCode(logistics.getCountryCode());
+            this.setCityCode(logistics.getCityCode());
+            this.setFedexBillTc(logistics.getFedexBillTc());
+            this.setFedexBillDt(logistics.getFedexBillDt());
+            if(logistics.getDeliveryDate() != null)
+                this.setDeliveryDate(String.valueOf(logistics.getDeliveryDate()));
+            this.courierDetails = new CourierDetailDTO(logistics.getCourier());
+        }
 
-	public String getServiceType() {
-		return serviceType;
-	}
+    }
 
-
-
-	public void setServiceType(String serviceType) {
-		this.serviceType = serviceType;
-	}
+    public String getServiceType() {
+        return serviceType;
+    }
 
 
 
-	public String getPaymentType() {
-		return paymentType;
-	}
+    public void setServiceType(String serviceType) {
+        this.serviceType = serviceType;
+    }
 
 
 
-	public void setPaymentType(String paymentType) {
-		this.paymentType = paymentType;
-	}
+    public String getPaymentType() {
+        return paymentType;
+    }
 
 
 
-	public BigDecimal getCODAmount() {
-		return CODAmount;
-	}
+    public void setPaymentType(String paymentType) {
+        this.paymentType = paymentType;
+    }
 
 
 
-	public void setCODAmount(BigDecimal cODAmount) {
-		CODAmount = cODAmount;
-	}
+    public BigDecimal getCODAmount() {
+        return CODAmount;
+    }
 
 
 
-	public String getCourierStatus() {
-		return courierStatus;
-	}
+    public void setCODAmount(BigDecimal cODAmount) {
+        CODAmount = cODAmount;
+    }
 
 
 
-	public void setCourierStatus(String courierStatus) {
-		this.courierStatus = courierStatus;
-	}
+    public String getCourierStatus() {
+        return courierStatus;
+    }
 
 
 
-	public String getLmsStatus() {
-		return lmsStatus;
-	}
+    public void setCourierStatus(String courierStatus) {
+        this.courierStatus = courierStatus;
+    }
 
 
 
-	public void setLmsStatus(String lmsStatus) {
-		this.lmsStatus = lmsStatus;
-	}
+    public String getLmsStatus() {
+        return lmsStatus;
+    }
 
 
 
-	public String getCourierName() {
-		return courierName;
-	}
+    public void setLmsStatus(String lmsStatus) {
+        this.lmsStatus = lmsStatus;
+    }
 
 
 
-	public void setCourierName(String courierName) {
-		this.courierName = courierName;
-	}
+    public String getCourierName() {
+        return courierName;
+    }
 
 
 
-	public String getCourierCode() {
-		return courierCode;
-	}
+    public void setCourierName(String courierName) {
+        this.courierName = courierName;
+    }
 
 
 
-	public void setCourierCode(String courierCode) {
-		this.courierCode = courierCode;
-	}
+    public String getCourierCode() {
+        return courierCode;
+    }
 
 
 
-	public String getAirwayBillNumber() {
-		return airwayBillNumber;
-	}
+    public void setCourierCode(String courierCode) {
+        this.courierCode = courierCode;
+    }
 
 
 
-	public void setAirwayBillNumber(String airwayBillNumber) {
-		this.airwayBillNumber = airwayBillNumber;
-	}
+    public String getAirwayBillNumber() {
+        return airwayBillNumber;
+    }
 
 
 
-	public String getPromiseDate() {
-		return promiseDate;
-	}
+    public void setAirwayBillNumber(String airwayBillNumber) {
+        this.airwayBillNumber = airwayBillNumber;
+    }
 
 
-	public void setPromiseDate(String promiseDate) {
-		this.promiseDate = promiseDate;
-	}
+
+    public String getPromiseDate() {
+        return promiseDate;
+    }
+
+
+    public void setPromiseDate(String promiseDate) {
+        this.promiseDate = promiseDate;
+    }
 
 
     public String getDeliveryDate() {
@@ -183,13 +189,13 @@ public class LogisticsDetailsDTO {
 
 
     public String getTrackingLink() {
-		return trackingLink;
-	}
+        return trackingLink;
+    }
 
 
-	public void setTrackingLink(String trackingLink) {
-		this.trackingLink = trackingLink;
-	}
+    public void setTrackingLink(String trackingLink) {
+        this.trackingLink = trackingLink;
+    }
 
 
     public String getBarCode() {
